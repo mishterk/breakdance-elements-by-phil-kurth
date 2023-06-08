61c22b435bcfb4bc20c131440de973392a3e932e
Phil Kurth <phil@philkurth.com.au>
2023-06-09 08:15:50 +1000

    update plugin base

--- plugin.php
+++ plugin.php
@@ -1,46 +1,46 @@
 <?php
 
 /**
- * Plugin Name: Breakdance Custom Elements
- * Plugin URI: https://breakdance.com/
- * Description: ALPHA - NOT TO BE USED IN PRODUCTION
- * Author: Breakdance
- * Author URI: https://breakdance.com/
+ * Plugin Name: PDK | Breakdance Custom Elements
+ * Plugin URI: https://github.com/mishterk/pdk-breakdance-custom-elements
+ * Description: A custom element library for the Breakdance builder
+ * Author: Phil Kurth
+ * Author URI: https://philkurth.com.au
  * License: GPLv2
- * Text Domain: breakdance
+ * Text Domain: pdk-breakdance-custom-elements
  * Domain Path: /languages/
  * Version: 0.0.1
  */
 
-namespace BreakdanceCustomElements;
+namespace PdkBreakdanceCustomElements;
 
 use function Breakdance\Util\getDirectoryPathRelativeToPluginFolder;
 
-add_action('breakdance_loaded', function () {
-    \Breakdance\ElementStudio\registerSaveLocation(
-        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/elements',
-        'BreakdanceCustomElements',
-        'element',
-        'Custom Elements',
-        false
-    );
+add_action( 'breakdance_loaded', function () {
+	\Breakdance\ElementStudio\registerSaveLocation(
+		getDirectoryPathRelativeToPluginFolder( __DIR__ ) . '/elements',
+		'PdkBreakdanceCustomElements',
+		'element',
+		'Custom Elements',
+		false
+	);
 
-    \Breakdance\ElementStudio\registerSaveLocation(
-        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/macros',
-        'BreakdanceCustomElements',
-        'macro',
-        'Custom Macros',
-        false,
-    );
+	\Breakdance\ElementStudio\registerSaveLocation(
+		getDirectoryPathRelativeToPluginFolder( __DIR__ ) . '/macros',
+		'PdkBreakdanceCustomElements',
+		'macro',
+		'Custom Macros',
+		false,
+	);
 
-    \Breakdance\ElementStudio\registerSaveLocation(
-        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/presets',
-        'BreakdanceCustomElements',
-        'preset',
-        'Custom Presets',
-        false,
-    );
+	\Breakdance\ElementStudio\registerSaveLocation(
+		getDirectoryPathRelativeToPluginFolder( __DIR__ ) . '/presets',
+		'PdkBreakdanceCustomElements',
+		'preset',
+		'Custom Presets',
+		false,
+	);
 },
-    // register elements before loading them
-    9
+	// register elements before loading them
+	9
 );
